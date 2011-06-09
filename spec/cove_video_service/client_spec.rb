0abfadd15bcb08c522da60a52a5062b4bf880eb6
Ryan Weald <ryan@weald.com>
2011-06-08 21:53:48 -0700

    add another basic spec to test custom hostname

--- spec/cove_video_service/client_spec.rb
+++ spec/cove_video_service/client_spec.rb
@@ -13,6 +13,12 @@ describe CoveVideoService::Client do
       Client.expects(:request_token).returns("hello")
       Client.generate_url("blah.webm").should == "http://127.0.0.1:5678/videos/hello.webm"
     end
+
+    it "should allow you to manually set the host" do
+      Client.host = "http://chai.ucsd.edu"
+      Client.expects(:request_token).returns("hello")
+      Client.generate_url("blah.webm").should == "http://chai.ucsd.edu/videos/hello.webm"
+    end
   end
 
 end
