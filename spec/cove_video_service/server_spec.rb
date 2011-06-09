79d042454d0c78bc7dd93b5fcbc6e1932eaa7bac
Ryan Weald <ryan@weald.com>
2011-06-08 19:10:22 -0700

    add the basic executable that will run the server

--- spec/cove_video_service/server_spec.rb
+++ spec/cove_video_service/server_spec.rb
@@ -11,6 +11,23 @@ describe CoveVideoService do
         get '/generate_token'
         last_response.status.should == 400
       end
+
+      it "should return a notice that you must specify a video if no video given" do
+        parse_json_response do
+          get '/generate_token'
+        end
+        @response["notice"].should be
+      end
+
+      it "should return 200 if valid video given" do
+        get '/generate_token', :video => "test.webm"
+        last_response.status.should == 200
+      end
+
+      it "shoud return a token if valid video param given" do
+        parse_json_response { get "/generate_token", :video => "test.webm"}
+        @response["token"].should be
+      end
       
     end
   end
