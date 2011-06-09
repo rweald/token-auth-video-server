446aeef8d440bbb20d88cd84034c5a6b4308fa3d
Ryan Weald <ryan@weald.com>
2011-06-09 14:00:01 -0700

    make it so the token request timeout is configurable

--- lib/token-auth-video-server/client.rb
+++ lib/token-auth-video-server/client.rb
@@ -9,10 +9,10 @@ module TAVideoServer
     @url_host = "http://127.0.0.1:4250"
     @token_host = "http://127.0.0.1:4250"
 
-    def self.generate_url(video_name)
+    def self.generate_url(video_name, timeout=1.5)
       format = video_name.split(".")[1] || "webm"
       begin
-        token = self.request_token(video_name)
+        token = self.request_token(video_name, timeout)
       rescue
         puts "token request failed or timedout"
         token = "nil"
@@ -20,11 +20,11 @@ module TAVideoServer
       @url_host + "/videos/" + "#{token}.#{format}"
     end
 
-    def self.request_token(video_name)
+    def self.request_token(video_name, timeout)
       puts "begining token request"
 
       url = @token_host + "/generate_token" + "?video=#{video_name}"
-      resp = Restclient::Request.execute(:method => :get, :url => url, :timeout => 1.5)
+      resp = Restclient::Request.execute(:method => :get, :url => url, :timeout => timeout)
       resp = JSON.parse(resp.body)
 
       puts "token request succeeded"
